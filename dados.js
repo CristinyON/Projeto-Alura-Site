11b9e246d635a64ad0a9266bef86be0a3d408d46
CristinyON <cristinybarbo@gmail.com>
2024-09-06 19:56:23 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Marceline", 
+        descricao : "Marceline Abadeer é um dos personagens mais icônicos e complexos da série animada Hora de Aventura. Como uma vampira adolescente imortal, ela contrasta com a inocência de Finn e Jake, trazendo uma profundidade emocional e um toque de rock'n'roll à série. Apesar de sua aparência sombria e de sua natureza vampírica, Marceline possui um coração gentil e uma paixão pela música. Sua relação com a Princesa Jujuba é um dos aspectos mais explorados da série, revelando camadas de sua personalidade e passado. Marceline é conhecida por sua guitarra Hambo e por suas canções melancólicas, que refletem suas experiências de vida e seus sentimentos mais profundos.", 
+        link : "https://www.youtube.com/watch?v=oY5zyHpMXtI",
+        tags: "vampira rei gelado jujuba"
+    }, 
+    {
+        titulo : "Finn", 
+        descricao : "Finn o Humano é o protagonista da série Hora de Aventura. Um menino humano que vive em uma terra pós-apocalíptica chamada Ooo, Finn embarca em diversas aventuras com seu melhor amigo e irmão adotivo, Jake, um cão mágico com a capacidade de se esticar.", 
+        link : "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwj3kPza7aeIAxXiupUCHVmOL44QFnoECEsQAQ&url=https%3A%2F%2Fhoradeaventura.fandom.com%2Fpt-br%2Fwiki%2FFinn&usg=AOvVaw28LbANuF8yKocN4jfYali6&opi=89978449",
+        tags: "jake rainha de fogo jujuba"
+    }, 
+    {
+        titulo : "Jujuba", 
+        descricao : "A Princesa Jujuba é uma das personagens mais inteligentes e doces de Ooo. Uma princesa doce e inteligente, ela é a governante do Reino Doce e uma grande amiga da Marceline.", 
+        link : "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjJ_Neo7qeIAxWGqJUCHcVqGWIQwqsBegQIShAG&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DOxmieTvN63o&usg=AOvVaw3V4KxJjLZM0wqN7gBYt-B1&opi=89978449",
+        tags: "marceline reino doce menta princesa caroço"
+    }
+];
